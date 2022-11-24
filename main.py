c652b2d0c5767c155169d014af7fa39495d81892
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-11-24 20:33:32 +0900

    added main.py, object.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import objeckt
+import pygame
+
+
+
+
+SCREEN_SIZE = (960,640)
+clock=pygame.time.Clock()
+
+def init():
+    pygame.init()
+    pygame.display.set_caption("なっでなで")
+    window = pygame.display.set_mode(SCREEN_SIZE)
+
+    player = objeckt.Player(10,000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+    enemy = objeckt.Enemy(100,2)
+    return player,enemy
+
+def main():
+    running=True
+    player,enemy=init()
+    while running:
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                running =False
+            elif event.type == pygame.KEYDOWN:
+                if event.key== pygame.K_RETURN:
+                    player
+                    
+                    
+        clock.tick(30)
+
+init()
+main()
+
---
+++ objeckt.py
@@ -0,0 +1,35 @@
+import random
+import pygame
+
+
+class Enemy():
+    
+    def __init__(self,hp,kaihuku) -> None:
+        self.hp=hp
+        self.kaihuku=kaihuku
+        
+    def get_attack():
+        enemy.hp -= 5
+        
+    def X_attck(self,player):
+        player.hp -= random.randrange(0,int(player.hp-1,5),3)
+
+class Player():
+    
+    def __init__(self,hp,speed) -> None:
+        self.hp=0
+        self.speed=0
+        
+    def attack(self,enemy:Enemy):
+        enemy.get_attack()
+        
+        
+    def die(self):
+        self.hp = 0
+        print("Game Over!")
+        
+    def is_winer(self,enemy:Enemy):
+        if enemy.hp <= 10:
+            print("Your winer")
+        else:
+            pass
