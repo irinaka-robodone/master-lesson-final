a3af031e9339cd377f771143e743a9779a1a49e5
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-12-01 20:08:38 +0900

    updated main.py, object.py, added utils.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import objeckt
 import pygame
-
-
-
+import utils
 
 SCREEN_SIZE = (960,640)
 clock=pygame.time.Clock()
@@ -12,7 +10,7 @@ def init():
     pygame.display.set_caption("なっでなで")
     window = pygame.display.set_mode(SCREEN_SIZE)
 
-    player = objeckt.Player(10,000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+    player = objeckt.Player(100,1)
     enemy = objeckt.Enemy(100,2)
     return player,enemy
 
@@ -22,15 +20,20 @@ def main():
     while running:
         for event in pygame.event.get():
             
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:       
                 running =False
             elif event.type == pygame.KEYDOWN:
                 if event.key== pygame.K_RETURN:
-                    player
-                    
-                    
+                    player.attack(enemy)
+                    enemy.X_attck(player)
+                    print("player attack!!")
+        who_win=utils.get_who_win([player,enemy])
+
+        #print(who_win)
+        if who_win != None:
+            running=False
+            
         clock.tick(30)
 
-init()
+# init()
 main()
-
--- objeckt.py
+++ objeckt.py
@@ -7,18 +7,22 @@ class Enemy():
     def __init__(self,hp,kaihuku) -> None:
         self.hp=hp
         self.kaihuku=kaihuku
+        self.type="Enemy"
         
-    def get_attack():
-        enemy.hp -= 5
-        
-    def X_attck(self,player):
-        player.hp -= random.randrange(0,int(player.hp-1,5),3)
+    def get_attack(self):
+        self.hp -= 5
+        print("敵hp:",self.hp)
 
+    def X_attck(self,player):
+        player.hp -= random.randrange(0,int(player.hp*1.5),3)
+        print("自分hp",player.hp)
+        
 class Player():
     
     def __init__(self,hp,speed) -> None:
-        self.hp=0
-        self.speed=0
+        self.hp=hp
+        self.speed=speed
+        self.type="Player"
         
     def attack(self,enemy:Enemy):
         enemy.get_attack()
