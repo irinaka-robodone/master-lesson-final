96795c0f325862171f8f944b04fe6c3961b3877f
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-12-22 20:13:26 +0900

    update utils.py

--- lesson_17~24/utils.py
+++ lesson_17~24/utils.py
@@ -1,12 +1,26 @@
 import csv
 import pprint
 import object
+from object import Puizu
 
-with open('bun.csv', encoding="utf-8") as f:
-    print(f.read())
-    reader=csv.reader(f)
-    for row in reader:
-        print(row)
-        puizu=Puizu()
-        puizu.puizu=row()
-        row(1)
+def puizuBOX():
+    puizus=[]
+    with open('bun.csv', encoding="utf-8") as f:
+        #print(f.read())
+        reader=csv.reader(f)
+
+        for i,row in enumerate(reader):
+            #print(row)
+            if i < 1:
+                pass
+            else:
+                puizu=Puizu()
+                puizu.question=row[0]
+                puizu.type=row[1]
+                puizu.answer=row[2]
+                puizu.choices=row[3]
+                print(puizu)
+                puizus.append([puizu])
+    return puizus
+puizues=puizuBOX()
+print(puizues)
