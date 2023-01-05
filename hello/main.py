9798c7eaceb7e7183cca6e380bca47ea63377a48
passive-radio <srccreator@gmail.com>
2023-01-05 21:20:05 +0900

    add theme.json, update main.py

--- hello/main.py
+++ hello/main.py
@@ -1,28 +1,52 @@
 import pygame
+import sys
 from pygame.locals import *
 from text import Text
-
-
-# def draw_text(text: str) -> None:
-
+import pygame_gui
 
 SCREEN_SIZE=(900,600)
+FPS = 60
+clock = pygame.time.Clock()
+
+def show_text(window, text):
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # running = False
+                pygame.quit()
+                sys.exit()
+        window.fill((255,255,255))
+        new_text = pygame.font.SysFont("Noto Sans JP", 20).render(text, True, (10,10,10))
+        new_text_rect = new_text.get_rect(center=(SCREEN_SIZE[0]//2, SCREEN_SIZE[1]//2))
+        window.blit(new_text, new_text_rect)
+        pygame.display.flip()
+        pygame.display.update()
+        clock.tick(FPS)
 
 def init():
     pygame.init()
     pygame.display.set_caption("しりとりゲーム")
-    Window = pygame.display.set_mode(SCREEN_SIZE)
-    Window.fill((255,255,255))    
-def mian():
+    window = pygame.display.set_mode(SCREEN_SIZE)
+    window.fill((255,255,255))
+    
+    return window
+def main():
 
-    init()
-    clook = pygame.time.Clock()
-    ckock = pygame.time.Clock()
+    window = init()
+    clock = pygame.time.Clock()
+    manager = pygame_gui.UIManager(
+        SCREEN_SIZE,
+        theme_path="theme.json"
+    )
+    input_box = pygame_gui.elements.UITextEntryLine(
+        pygame.Rect(SCREEN_SIZE[0]//2 - 500//2, SCREEN_SIZE[1]//2 - 30//2, 500, 40),
+        manager,
+        object_id="#main_text_entry"
+    )
 
     running = True 
     while running:
-        pygame.display.flip()
-        ckock.tick(40)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,21 +54,17 @@ def mian():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     pass
-            elif event.type == pygame.KEYDOWN and not text.is_editing:
-                if event.key in event_trigger.keys():
-                    input_text = pygame.event_trigger[event.key]()
-                # 入力の確定
-                if event.unicode in ("\r", "") and event.key == pygame.K_RETURN:
-                    print(input_text)  # 確定した文字列を表示
-                    draw_text(format(text))  # テキストボックスに"|"を表示
-                    input_text = format(text)  # "|"に戻す
-                    break
-            elif event.type == pygame. TEXTEDITING:  # 全角入力
-                input_text =  pygame. pygame.text.edit(event.text, event.start)
-            elif event.type == pygame.TEXTINPUT:  # 半角入力、もしくは全角入力時にenterを押したとき
-                input_text =  pygame.text.input(event.text)
-            # 描画しなおす必要があるとき
-            if event.type in [KEYDOWN, TEXTEDITING, TEXTINPUT]:
-                draw_text(input_text)
+            elif event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
+                show_text(window, event.text)
+            manager.process_events(event)
+        
+        time_delta = clock.tick(FPS)/ 1000.0
+        manager.update(time_delta)
+        manager.draw_ui(window)
+        
+        pygame.display.flip()
+        pygame.display.update()
+        clock.tick(FPS)
 
-mian()
+
+main()
