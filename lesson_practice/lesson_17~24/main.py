1f73b209a6525116c7759c0eb9ab01931d0d861f
Irinaka Robodone <irinaka.robodone@gmail.com>
2023-01-19 20:10:45 +0900

    add world.py, update main.py

--- lesson_practice/lesson_17~24/main.py
+++ lesson_practice/lesson_17~24/main.py
@@ -4,7 +4,8 @@ import pygame
 import random
 from utils import puizuBOX
 
-
+goke=0
+running = True
 pygame.init()
 screen=pygame.display.set_mode((400,300))
 pygame.display.set_caption("keyboard event")
@@ -19,32 +20,59 @@ puizu = puizues[puizuID]
 
 print(puizues.pop(puizuID))
 
+
+
 while True:
-    screen.fill((0,0,0))
-    print(f"{puizuID}/{len(puizues)}")
-    
     for event in pygame.event.get():
-        if event.type ==K_SPACE:
-            pygame.quit()
-            sys.exit()
-            
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-            
-        if event.type == KEYDOWN:  # キーを押したとき
-            # ESCキーならスクリプトを終了
-            if event.key ==K_ESCAPE :
+            if event.type ==K_SPACE:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            if event.type == KEYDOWN:  
+                if event.key ==K_ESCAPE :
+                        while running:
+                            screen.fill((0,0,0))
+                            print(f"{puizuID}/{len(puizues)}")
+                            
+                            for event in pygame.event.get():
+                                if event.type ==K_SPACE:
+                                    pygame.quit()
+                                    sys.exit()
+                                    
+                                if event.type == pygame.QUIT:
+                                    pygame.quit()
+                                    sys.exit()
+                                    
+                                if event.type == KEYDOWN:  # キーを押したとき
+                                    # ESCキーならスクリプトを終了
+                                    if event.key ==K_ESCAPE :
+                                        pygame.quit()
+                                        sys.exit()
 
-            else:
-                puizuID=random.randint(0,len(puizues)-1)
-                puizu = puizues[puizuID]
-                print(puizues.pop(puizuID))
+                                    else:
+                                        puizuID=random.randint(0,len(puizues)-1)
+                                        puizu = puizues[puizuID]
+                                        print(puizues.pop(puizuID))
+                                        goke += 1
+                                    
+                            if goke == 2:
+                                running=False
+                                            
+                            Q=font.render(puizu.question,True, (255,255,255))
+                            screen.blit(Q,(10,10))
+                                        
 
-    Q=font.render(puizu.question,True, (255,255,255))
-    screen.blit(Q,(10,10))
-#"押されたキー = " + pygame.key.name(event.key),
-    pygame.display.flip()
-    pygame.display.update() 
+                        #"押されたキー = " + pygame.key.name(event.key),
+                            pygame.display.flip()
+                            pygame.display.update() 
+                                        
+                                    
+                        else:
+                            pass
+                                    #Home=font.render("please",True,(100,100,100))
+                                    #screen.bilt(Home,(10,10))
+                                    
+                                
---
+++ lesson_practice/lesson_17~24/wold.py
@@ -0,0 +1,24 @@
+class Wold():
+    def __init__(self) -> None:
+        self.now_scene="start"
+        self.now_puizu_nom=0
+    def process(self):
+        if self.now_scence=="start":
+            """
+            スタート画面用の関数を実行する
+            """
+            
+        elif self.game_scence=="game":
+            """
+            クイズゲームをプレイする用の関数を実行する
+            """
+
+
+
+
+
+
+
+
+
+
