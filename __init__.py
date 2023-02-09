b0001f113b2309eb5a9c8f469bb178152a4d9c88
passive-radio <srccreator@gmail.com>
2023-02-09 21:48:12 +0900

    update game.py, __init__.py

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from try_ecs import *
---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+from try_ecs.ecs.world import World
+from try_ecs.ecs.component import *
+from try_ecs.ecs.system import *
+import pytmx
+
+# from try_ecs.level import Level
+
+def main():
+    """
+    write something
+    """
+
+
+if __name__ == '__main__':
+    main()
+    world = World()
+    print(world)
