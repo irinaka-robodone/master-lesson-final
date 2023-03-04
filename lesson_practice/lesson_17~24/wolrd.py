625e1037ea48c3cc4eef64f707471189c455f4ad
Irinaka Robodone <irinaka.robodone@gmail.com>
2023-03-04 11:12:23 +0900

    update world.py

--- lesson_practice/lesson_17~24/wolrd.py
+++ lesson_practice/lesson_17~24/wolrd.py
@@ -38,8 +38,7 @@ class Wold():
         self.b = self.puizu.choices[1]
         self.c = self.puizu.choices[2]
         self.d = self.puizu.choices[3]
-        self.final_answer=0
-        self.tou_after=0
+        self.tou_answer=0
         
         self.Q=self.font.render(self.puizu.question,True, (255,255,255))
     def start(self):
@@ -94,21 +93,24 @@ class Wold():
                     sys.exit()
             
                 if event.key == K_a:
-                    self.tou = "1"
+                    self.tou_answer = "1"
                 if event.key == K_b:
-                    self.tou = "2"
+                    self.tou_answer = "2"
                 if event.key == K_c:
-                    self.tou = "3"
+                    self.tou_answer = "3"
                 if event.key == K_d:
-                    self.tou = "4"
+                    self.tou_answer = "4"
+                if event.key in (K_a, K_b,K_c,K_d):
+                    self.now_scene="jazzi"
                 # if event.key ==K_RETURN:
                 #     self.reply()
-                else:
-                    puizuID=random.randint(0,len(self.puizues)-1)
-                    self.puizu = self.puizues[puizuID]
-                    self.Q=self.font.render(self.puizu.question,True, (255,255,255))
-                    print(self.puizues.pop(puizuID))
-                    self.goke += 1
+                
+                    # puizuID=random.randint(0,len(self.puizues)-1)
+                    # self.puizu = self.puizues[puizuID]
+                    # self.Q=self.font.render(self.puizu.question,True, (255,255,255))
+                    # self.puizues.pop(puizuID)
+                    #self.goke += 1
+        print(self.tou, self.puizu.answer)
                 
         if self.goke == self.Xmon:
             self.running=False
@@ -125,12 +127,11 @@ class Wold():
         pygame.display.update()
     
     
-    
-    
+        
     def reply(self):
         if self.tou == self.puizu.answer:
             print("yes")
-            self.tou_after=self.tou
+            self.tou_answer=self.tou
             # self.Yes=self.font.render("正解",True, (255,255,115))
             # self.screen.blit(self.Yes,(300,200))
             
@@ -143,7 +144,8 @@ class Wold():
             # self.screen.blit(self.No,(300,100))
             
             self.tou=0
-        self.now_scene="jazzi"
+        
+            
         
     def handan(self):
         self.screen.fill((0,0,0))
@@ -160,18 +162,33 @@ class Wold():
                 #     if event.key ==K_0:
                 #         self.now_scene="game"
             
-        if self.tou_after == self.puizu.answer:
+        if self.tou_answer == self.puizu.answer:
             print("yes")
             self.yesno=self.font.render("正解",True, (255,255,115))
             #self.screen.blit(self.Yes,(300,200))            
-            
+            self.puizuID=random.randint(0,len(self.puizues)-1)
+            self.puizu = self.puizues[self.puizuID]
+            self.Q=self.font.render(self.puizu.question,True, (255,255,255))
+            self.puizues.pop(self.puizuID)
+            self.goke += 1
+            self.now_scene="game"
+        
         else:
-            print(self.tou_after)
+            print("No!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             self.yesno=self.font.render("不正解",True, (215,255,255))
-        self.screen.blit(self.yesno,(300,100))
-        self.update_display()
-        pygame.time.wait(2000)
-        self.now_scene="game"
+            self.screen.blit(self.yesno,(300,100))
+            self.update_display()
+            pygame.time.wait(2000)
+            print("test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        
+            self.puizuID=random.randint(0,len(self.puizues)-1)
+            self.puizu = self.puizues[self.puizuID]
+            self.Q=self.font.render(self.puizu.question,True, (255,255,255))
+            self.puizues.pop(self.puizuID)
+            self.goke += 1
+            self.now_scene="game"
+        
+        
         
     def process(self):
         if self.now_scene=="start":
@@ -182,8 +199,9 @@ class Wold():
             """
         elif self.now_scene=="game":
             self.main()
+            
             self.sentaku()
-            self.reply()
+            # self.reply()
             self.update_display()
             
             
